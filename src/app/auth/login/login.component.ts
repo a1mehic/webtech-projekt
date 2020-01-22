158a3336aa44680e16cb93cdc95ce7a8d22af18f
marleo <mariole@edu.aau.at>
2020-01-22 18:38:16 +0100

    Added error popup for wrong login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
+
   isLoading = false;
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router, private snackBar: MatSnackBar) {}
 
   onLogin(form: NgForm) {
+    this.isLoading = true;
     if (form.invalid) {
+      this.snackBar.open('Wrong User', 'Close', {
+        duration: 3000,
+        panelClass: ['errorPopup']
+      }); 
+      this.router.navigate(['/']);
       return;
     }
-    this.isLoading = true;
     this.authService.login(form.value.email, form.value.passwort);
+    this.router.navigate(['/']);
   }
 }
